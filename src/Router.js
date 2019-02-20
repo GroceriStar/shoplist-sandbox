0962903cadeca4848ca4f47e24bcae93de18832e
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-02-20 19:23:23 +0200

    ++

--- src/Router.js
+++ src/Router.js
@@ -2,15 +2,16 @@ import React, {
   Component
 } from 'react';
 import {
+  BrowserRouter,
   Switch,
   Route
 } from 'react-router-dom';
 
-import { 
-  Examples, DeleteLink, EditLink
+import {
+  Examples,
+  DeleteLink,
+  EditLink
 } from '@groceristar/shoplist-blocks-component'
-// import DeleteLink  from './views/Examples/DeleteLink';
-// import EditLink    from './views/Examples/EditLink';
 
 
 
@@ -19,12 +20,14 @@ class OldRouter extends Component {
   render() {
     console.log("this is");
     return (
-      <Switch>
-        <Route path='/'    component={Examples} exact />
-        <Route path='/examples'    component={Examples} exact />
-        <Route path='/examples/delete/:id' component={DeleteLink} />
-        <Route path='/examples/edit/:id' component={EditLink} />
-      </Switch>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/'    component={Examples} exact />
+          <Route path='/examples'    component={Examples} exact />
+          <Route path='/examples/delete/:id' component={DeleteLink} />
+          <Route path='/examples/edit/:id' component={EditLink} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 
