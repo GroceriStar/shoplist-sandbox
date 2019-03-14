ea02c076014d0fad7b77db1404cc3b5f9936bc2c
Vadim <vadim.putrov@gmail.com>
2019-03-14 21:30:10 +0200

    add Empty

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -11,7 +11,7 @@ import "antd/dist/antd.css";
 // import Main   from '@groceristar/antd-showcase-components';
 
 import { getIngredients } from "../selectors/selector";
-
+import { Em }   from '@groceristar/cards-wrapper';
 
 class HomeView extends Component {
 
@@ -19,20 +19,30 @@ class HomeView extends Component {
       // data: getGroceryCollection(), //Did we need it here? i think we didn use all data from this method
 
     };
+    getRecipes(recipes){
+      if(recipes != undefined && recipes.length > 0){
+        return ({recipes})
+      } else {
+        return (<Em />)
+      }
+    }
 
   render() {
 
 
     // const data = this.state.data;
+    // let fiveRecipes = getFirstFiveRecipes()
+
+
 
     return (
       <Fragment>
         <div className="main">
 
             {/*<Header />*/}
-            {/*
-            <Main data={getFirstFiveRecipes()} />
-            */}
+
+            {this.getRecipes()}
+
         </div>
       </Fragment>
 
